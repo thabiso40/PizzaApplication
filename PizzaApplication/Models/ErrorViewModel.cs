cce6f66b7153ebce8afca23badfccb0979842b29
Thabiso <gshado057@gmail.cim>
2020-06-25 23:55:10 +0200

    Add project files.

---
+++ PizzaApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PizzaApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
